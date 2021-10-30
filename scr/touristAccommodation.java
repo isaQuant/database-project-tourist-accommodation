2c8f3023663da6f68b10bb9bacfd93856581b536
isaQuant <isabella.quantschnig@gmx.net>
2021-10-30 19:08:08 +0200

    Added methods getTouristAccommodationID() fetchTouristAccommodationID() containsTouristAccommodationID
    for getting and setting the tourist accommodation ID which is automatically generated in the database when a new entry is added

--- scr/touristAccommodation.java
+++ scr/touristAccommodation.java
@@ -49,4 +49,107 @@ public class touristAccommodation {
     private PreparedStatement getEntityIDStmt;
 
 
+    // Getter and Setter
+
+    /**
+     * Gets the ID of the tourist accommodation from the database.
+     * The ID is generated automatically when a tourist accommodation is added to the database.
+     * @return the ID of the tourist accommodation
+     */
+    public Integer getTouristAccommodationID() {
+        if (this.touristAccommodationID == null && containsTouristAccommodationAttributes(this.touristAccommodationName, this.phone, this.email, this.fax, this.website, this.adress, this.accommodationType, this.numberOfStars)) {
+            fetchTouristAccommodationID(this.touristAccommodationName, this.phone, this.email, this.fax, this.website, this.adress, this.accommodationType, this.numberOfStars);
+            return this.touristAccommodationID;
+        }
+        return touristAccommodationID;
+    }
+
+    /**
+     * Fetches the tourist accommodation ID from the database
+     * @param touristAccommodationName
+     * @param phone
+     * @param email
+     * @param fax
+     * @param website
+     * @param adress
+     * @param accommodationType
+     * @param numberOfStars
+     */
+    private void fetchTouristAccommodationID(String touristAccommodationName, String phone, String email, String fax, String website, String adress, accommodationTypeEnum accommodationType, numberOfStarsEnum numberOfStars) {
+        try {
+            this.getIDStmt.setString(1, touristAccommodationName);
+            this.getIDStmt.setString(2, phone);
+            this.getIDStmt.setString(3, email);
+            this.getIDStmt.setString(4, fax);
+            this.getIDStmt.setString(5, website);
+            this.getIDStmt.setString(6, adress);
+            this.getIDStmt.setString(7, String.valueOf(accommodationType));
+            this.getIDStmt.setString(8, numberOfStars.getNumVal());
+
+            ResultSet rs = getIDStmt.executeQuery();
+            rs.next();
+            this.touristAccommodationID = rs.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    /************************ Start of contains-methods *******************************/
+
+
+
+    /**
+     * Checks if a tourist accommodation with specific given attributes is contained in the databaes
+     * @param touristAccommodationName
+     * @param phone
+     * @param email
+     * @param fax
+     * @param website
+     * @param adress
+     * @param accommodationType
+     * @param numberOfStars
+     * @return true if the tourist accommodation is contained in the database, else return false
+     */
+    private boolean containsTouristAccommodationAttributes(String touristAccommodationName, String phone, String email, String fax, String website, String adress, accommodationTypeEnum accommodationType, numberOfStarsEnum numberOfStars) {
+        try {
+            this.containsTouristAccommodationAttributesStmt.setString(1, touristAccommodationName);
+            this.containsTouristAccommodationAttributesStmt.setString(2, phone);
+            this.containsTouristAccommodationAttributesStmt.setString(3, email);
+            this.containsTouristAccommodationAttributesStmt.setString(4, fax);
+            this.containsTouristAccommodationAttributesStmt.setString(5, website);
+            this.containsTouristAccommodationAttributesStmt.setString(6, adress);
+            this.containsTouristAccommodationAttributesStmt.setString(7, String.valueOf(accommodationType));
+            this.containsTouristAccommodationAttributesStmt.setString(8, numberOfStars.getNumVal());
+
+            ResultSet rs = containsTouristAccommodationAttributesStmt.executeQuery();
+
+            if(rs.next()) {
+                rs.close();
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+
+
+
+
 }
